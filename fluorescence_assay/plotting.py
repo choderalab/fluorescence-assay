958869d9c63153ae15a5bca1bcb559b3920d80d5
Mark Polk <mark.polk@choderalab.org>
2024-11-29 17:42:28 -0500

    Started plotting module.

---
+++ fluorescence_assay/plotting.py
@@ -0,0 +1,64 @@
+"""Module to plot parsed plate reader ouptputs."""
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+from .plate_reader import Measurements, IControlXML
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from typing import Optional
+
+
+@dataclass
+class Plot(ABC):
+    """"""
+    
+    @abstractmethod
+    def load_data(self, Measurement: Measurements, *args, **kwargs) -> None:
+        """"""
+        ...
+
+    def format_plot(self, axes: matplotlib.axes._axes.Axes, xlim: Optional[list[float]] = None, ylim: Optional[list[float]] = None, xlabel: Optional[str] = None, ylabel: Optional[str] = None, square: Optional[bool] = False):
+        
+        if xlim is not None:
+            axes.set_xlim(xlim)
+
+        if ylim is not None:
+            axes.set_ylim(ylim)
+
+        if xlabel is not None:
+            axes.set_xlabel(xlabel)
+
+        if ylabel is not None:
+            axes.set_ylabel(ylabel)
+
+        if square == True:
+            axes.set_box_aspect(1)
+
+
+
+@dataclass
+class IControlXMLPlot(Plot):
+    """"""
+
+    _plate_read: dict = field(default_factory=dict, init=False)
+
+    def load_data(self, IControlXML: IControlXML) -> None:
+        
+        self._plate_read = IControlXML
+
+    def get_wavelength_axis(self, section) -> np.ndarray:
+
+        lmin, lmax, lstep = [self._plate_read.get_parameter(section, parameter) for parameter in ["Emission Wavelength Start", "Emission Wavelength End", "Emission Wavelength Step Size"]]
+
+        return np.arange(lmin, lmax + lstep, lstep)
+
+    def plot_well_spectrum(self, axes: matplotlib.axes._axes.Axes, section: str, well: str, cycle: Optional[int] = 1, color: Optional[tuple] = (0,0,0), label: Optional[str] = None) -> None:
+
+        data = np.array(list(self._plate_read.get_well(section, well, cycle).values()))
+
+        ll = self.get_wavelength_axis(section)
+
+        axes.plot(ll, data, color=color, label=label)
